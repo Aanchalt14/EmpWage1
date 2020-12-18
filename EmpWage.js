6058c6a7c8885991d7b4a245a441738c98138905
Aanchalt14 <aanchalt71@gmail.com>
2020-12-18 12:47:25 +0530

    Added : till UC-5

--- EmpWage.js
+++ EmpWage.js
@@ -51,3 +51,31 @@ let empCheck = Math.floor(Math.random() *10) %3;
 empHrs = getworkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("emp wage " +empWage);
+
+
+//UC-4
+const NUM_OF_WORKING_DAYS =2 ;
+let empHrs =0;
+for (let day=0; day<NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() *10)%3;
+    empHrs += getworkingHo
+urs(empCheck);
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total hrs :" +empHrs+ "Emp wage : " +empWage);
+
+
+
+//UC-5 
+const MAX_HRS_IN_MONTH =100;
+const NUM_OF_WORKING_DAYS =10;
+let totalEmpHrs =0;
+let totalWorkingDays=0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+    }
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total days : " +totalWorkingDays + "total hrs : "+totalEmpHrs + "Emp wage :" +empWage);
+
