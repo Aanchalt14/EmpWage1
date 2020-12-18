666d095456737ffe12698f2b8c780718861463a6
Aanchalt14 <aanchalt71@gmail.com>
2020-12-18 22:26:15 +0530

    Added: store day and daily wage along with totaql wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,81 +1,27 @@
-//UC-1 
-const IS_ABSENT =0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if ( empCheck == IS_ABSENT) {
-    console.log("Employee is absent");
-    return ;
-}
-else {
-     console.log("Employee is present");
-}
+//UC-8
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+let totalemphrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+let empDailyWagemap = new Map();
 
-
-//UC-2
-const IS_PART_TIME =1;
-const IS_FULL_TIME =2 ;
-const PART_TIME_HOURS=4;
-const FULL_TIME_HOURS =8;
-const WAGE_PER_HOUR=20;
-
-let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case 1 : 
-        empHrs = 1;
-        break;
-    case 2:
-        emphrs = 2;
-        break;
-    default:
-        empHrs=0;
+function calcDailyWage(empHrs) {
+  return empHrs * WAGE_PER_HOUR;
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage :" +empWage);
-
-
-//UC-3
-function getworkingHours(empCheck) {
-    switch  (empCheck) {
-        case IS_PART_TIME :
-            return PART_TIME_HOURS;
-        case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
-        default:
-            return 0;
-    }
+while (totalEmpHrs <= MAZX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+totalWorkingDays++ ;
+let empCheck = Math.floor(Math.random() * 10 ) % 3;
+let empHrs = getWorkingHours (empCheck);
+totalEmpHrs += empHrs ;
+empDailyWageArr.push(calcDailyWage(empHrs));
+empDailyWageMap.set(totalWorkingDays , calcDailyWage(empHrs));
 }
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() *10) %3;
-empHrs = getworkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("emp wage " +empWage);
-
-
-//UC-4
-const NUM_OF_WORKING_DAYS =2 ;
-let empHrs =0;
-for (let day=0; day<NUM_OF_WORKING_DAYS; day++){
-    let empCheck = Math.floor(Math.random() *10)%3;
-    empHrs += getworkingHo
-urs(empCheck);
+console.log(empDailyWageMap);
+function totalWages(totalWage , dailyWage) {
+ return totalWage + dailyWage;
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total hrs :" +empHrs+ "Emp wage : " +empWage);
-
-
-
-//UC-5 
-const MAX_HRS_IN_MONTH =100;
-const NUM_OF_WORKING_DAYS =10;
-let totalEmpHrs =0;
-let totalWorkingDays=0;
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
-    }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Total days : " +totalWorkingDays + "total hrs : "+totalEmpHrs + "Emp wage :" +empWage);
-
+console.log("UC7A - Emp wage map total Hrs "
+ +Array.from(empDailyWageMap.values()).reduce(totalWages , 0));
